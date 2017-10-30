9c9b60de1e6e2a918446f668e2d71ecb5b7cdf31
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-10-30 17:03:42 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var WelcomeMessage = prompt("Which calculator would you like to use. Type a for basic, b for basic and m for BMI")
+
+if WelcomeMessage = "a" {
+  var a = prompt("Enter your first number");
+} else {
+
+}
