b7320d2d95a44ec5e603b05cf456ab517e195f1b
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-10-30 20:23:25 +0000

    completed the javascript code for the basic calculator

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 
 var WelcomeMessage = prompt("Which calculator would you like to use. Type a for basic, b for basic and m for BMI")
 
-if WelcomeMessage = "a" {
+if ( WelcomeMessage == "a") {
   var a = prompt("Enter your first number");
+  var op = prompt("please choose from the following *, /, +, -");
+  var b = prompt("Enter your second number");
+
+  switch (op) {
+    case "*":
+    var multiply = a*b;
+    alert("Your answer is " + multiply);
+    break;
+    case "/":
+    var divide = a/b;
+    if ( b =="0") {
+      alert("Error!, Cannot divide by 0");
+    }else if (b != 0) {
+      alert("Your answer is " + divide);
+    }
+    break;
+    case "+":
+    var add = a+b;
+    alert("Your answer is " + add);
+    break;
+    case "-":
+    var minus = a-b;
+    alert("Your answer is " + minus);
+    break;
+    default:
+    console.log("Something went wrong!");
+  }
 } else {
+console.log("hey");
+}
+
+if () {
 
 }
