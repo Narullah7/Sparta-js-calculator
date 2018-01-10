d1d1d4b92f691c8bec217505ac41775354f2a4d3
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2018-01-10 20:51:55 +0000

    fixed mistakes

--- main.js
+++ main.js
@@ -1,87 +1,73 @@
+// Sparta Javascript Prompt Calculator
 
-var WelcomeMessage = prompt("Which calculator would you like to use. Type a for basic, b for advanced, m for BMI and t for trip calculator")
+// Added a prompt to ask the user to choose between advanced and basc calculator
+var whichCalculator = prompt("Which calculator would you like to use? Click (a) for advanced and (b) for basic (x) for BMI");
 
-if ( WelcomeMessage == "a") {
-  var a = prompt("Enter your first number");
-  var op = prompt("please choose from the following *, /, +, -");
-  var b = prompt("Enter your second number");
+// Advanced calculator (a)
+if (whichCalculator === 'a') {
 
-  switch (op) {
-    case "*":
-    var multiply = parseInt(a)*parseInt(b);
-    alert("Your answer is " + multiply);
-    break;
-    case "/":
-    var divide = parseInt(a)/parseInt(b);
-    if ( b =="0") {
-      alert("Error!, Cannot divide by 0");
-    }else if (b != 0) {
-      alert("Your answer is " + divide);
-    }
-    break;
-    case "+":
-    var add = parseInt(a)+parseInt(b);
-    alert("Your answer is " + add);
-    break;
-    case "-":
-    var minus = parseInt(a)-parseInt(b);
-    alert("Your answer is " + minus);
-    break;
-    default:
-    console.log("Something went wrong!");
-  }
-} else {
-console.log("hey");
+	// Prompt to get the user to choose between power and sqr operation
+	var powerOrSquareRoot = prompt("Please choose (p) for power function and (s) for squareroot");
+
+		// Power operation
+		if(powerOrSquareRoot === 'p') {
+			// Prompt to get the fixed base for the power function
+			var fixedBase = prompt("Please enter your fixed base");
+			// Prompt to ask what user wanted fixed base raised to
+			var raise = prompt("What would you like to raise " + fixedBase + " to?");
+			// Power calculation and turning strings in integer before calculation
+			var power = Math.pow(parseInt(fixedBase), parseInt(raise));
+			// Show the result
+			alert(fixedBase + " to the power of " + raise + " is " + power);
+
+		// Squareroot operation
+		} else if (powerOrSquareRoot === 's') {
+			// Prompt to get what number the user wants the squareroot of
+			var sqrtNum = prompt("What number would you like to get the square root of?");
+			// js Math method to find the sqrt of sqrtNum after its changed to integer;
+			var root = Math.sqrt(parseInt(sqrtNum));
+			// Show the result
+			alert ("The squareroot of " + sqrtNum + " is " + root);
+		} else {
+			alert("Error - Please select correct Calculator");
+		}
+
+// Basic calculator (b)
+} else if (whichCalculator === 'b') {
+
+	// Prompts to get calculation values and operator
+	var firstIntChoice = prompt("Please enter the first number in your calculation");
+	var operationChoice = prompt("What operator would you like to use? + - / *");
+	var secondIntChoice = prompt("Please enter the second number in your calculation");
+	var result = 0
+
+	// Control flow for the operators
+	// Added parseFloat to work with floats
+	if (operationChoice === "+") {
+		// Calculation for Additon
+		result = parseFloat(firstIntChoice) + parseFloat(secondIntChoice);
+	} else if (operationChoice === "-") {
+		// Calculation for Minus
+		result = parseFloat(firstIntChoice) - parseFloat(secondIntChoice);
+	} else if (operationChoice === "/") {
+		// Calculation for Divide
+		result = parseFloat(firstIntChoice) / parseFloat(secondIntChoice);
+	} else if (operationChoice === "*") {
+		// Calculation for Times
+		result = parseFloat(firstIntChoice) * parseFloat(secondIntChoice);
+	} else {
+		alert("Error");
+	}
+
+	// Show the result
+	alert(firstIntChoice + " " + operationChoice + " " + secondIntChoice + " = " + result);
+
+// BmiCalculator
+} else if (whichCalculator === 'x') {
+	var weight = prompt("To calculate your BMI please enter your weight in (kg) first!");
+	var height = prompt("Next please enter your height in (m) next!");
+	var topBmi = weight / height;
+	var bmi = topBmi / height;
+	// Show BMI
+	alert("Your BMI is " + bmi + " !");
 }
-
-if (WelcomeMessage == "b") {
-var op = prompt("Please choose from the following square-root or power");
-
-switch (op) {
-  case "square root":
-  var a = prompt("Please enter your number");
-  if (a < 0) {
-    alert("You cannot square-root a minus number")
-  } else if (a >= 0) {
-    var squareroot = Math.sqrt(a);
-    alert("Your answer is " + squareroot);
-  }
-  break;
-  case "power":
-  var a = prompt("please choose your base number");
-  var b = prompt("please choose your exponent");
-  var power = Math.pow(a,b);
-  alert("Your answer is " + power);
-  break;
-  default:
-  alert("Something went wrong!");
-
-}
-}
-
-if (WelcomeMessage == "m") {
-  var a = prompt("please enter your weight(kg)");
-  var b = prompt("please enter your height(m)")
-  var heimet = parseFloat(b);
-  var hsqr = Math.pow(heimet,2);
-  var bmi = parseInt(a) / hsqr;
-  alert("Your BMI is " + bmi);
-}
-
-if (WelcomeMessage == "t") {
-  var mpg = prompt("Enter your fuel efficiency(mpg)");
-  var cost = prompt("Enter your cost per gallon");
-  var distance = prompt("Enter the distance travelled");
-  var speed = prompt("Enter your speed(in hours)");
-
-  var time = (parseInt(distance)) / (parseInt(speed));
-  var totcost = parseInt(mpg) * parseFloat(cost);
-
-  if (distance > "60") {
-    var diff = (parseInt(distance) - 60)*2;
-    var Nmpg = (parseInt(mpg) - diff) * cost;
-    alert("Your trip will take " + time + "hours and will cost: £ " +Nmpg );
-  } else {
-    alert("Your trip will take " + time + "hours and will cost: £" + totcost)
-  }
-  }
